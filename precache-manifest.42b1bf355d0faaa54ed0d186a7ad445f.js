8303a2e3e3c3d2c02060d5abbf9d5afef025cc45
Angrey Shoful <shoful.a@gmail.com>
2020-10-12 13:58:09 +0300

    Updates

---
+++ precache-manifest.42b1bf355d0faaa54ed0d186a7ad445f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7593f5250006642891313eaa93d5d96",
+    "url": "/reactbigtablewebsql/index.html"
+  },
+  {
+    "revision": "d579c8663aa43b79d23e",
+    "url": "/reactbigtablewebsql/static/css/main.3cf119ef.chunk.css"
+  },
+  {
+    "revision": "9bc8a24895cfd6ed398a",
+    "url": "/reactbigtablewebsql/static/js/2.c3b143de.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reactbigtablewebsql/static/js/2.c3b143de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d579c8663aa43b79d23e",
+    "url": "/reactbigtablewebsql/static/js/main.3649a294.chunk.js"
+  },
+  {
+    "revision": "c3f246ff24816ff9afdb",
+    "url": "/reactbigtablewebsql/static/js/runtime-main.1b970c42.js"
+  }
+]);
